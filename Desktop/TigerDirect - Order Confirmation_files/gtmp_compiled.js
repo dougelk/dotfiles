243aeaee8e658b4ef340cee1ce4e597ecbef953f
dougelk <dougelk@gmail.com>
2015-12-05 16:38:55 -0500

    Several more deletions

--- Desktop/TigerDirect - Order Confirmation_files/gtmp_compiled.js
+++
@@ -1,1 +0,0 @@
-(function(){var d=document,S="script",s;gts=window.gts||[];gts.push(["jsv","0XbnmPGzfA8"]);gts.push(["gtmJsHost","www.googlecommerce.com\/trustedstores"]);gts.push(["sv","1"]);s=d.createElement(S);s.type="text/java"+S;s.async=!0;s.src="\/\/www.gstatic.com\/trustedstores\/js\/gtmp_compiled_0XbnmPGzfA8.js";S=d.getElementsByTagName(S)[0];S.parentNode.insertBefore(s,S);})();
